d23605a654eaa5855cac90e0f7225e396c66367b
oshani madhushika <oshanimadhushika12@gmail.com>
2024-03-23 18:20:31 +0530

    change db name

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
-$dbName = "my_login_register";
+$dbName = "spects_shop";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong;");
